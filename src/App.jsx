695cd27ac6ca52237977d7ef3778714c4c8dcfa1
Ifeanyi Aghasili <99535343+IFeanyI03@users.noreply.github.com>
2025-09-17 18:11:00 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import LandingPage from './pages/LandingPage';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 
+import EditGiftCardRates from './components/EditGiftCardRates;
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/signin" element={<SignInPage />} />
       <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
+    <Route path="/edit-000" element={<EditGiftCardRates />} />
     </Routes>
   );
 }
